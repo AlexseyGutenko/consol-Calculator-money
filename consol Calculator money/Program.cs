80ef74b60bc4fd88be3a1aa322158c5f04f0796b
guten <guten@DESKTOP-ASMODEU>
2024-01-12 09:54:21 +0300

    Добавьте файлы проекта.

---
+++ consol Calculator money/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consol_Calculator_money
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
